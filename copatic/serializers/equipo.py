cdd97700ef51c3701daed8c6df25ce3d6ce64aef
Iván Hoffmann <digital.h@gmail.com>
2018-06-06 09:49:50 -0300

    Agregando campos al serializer de equipos. Asignando insignias a actividades.

--- copatic/serializers/equipo.py
+++ copatic/serializers/equipo.py
@@ -5,4 +5,4 @@ class EquipoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Equipo
-        fields = ('id', 'eid', 'escuela', 'nombre', 'dt', 'puntos')
+        fields = ('id', 'eid', 'escuela', 'nombre', 'dt', 'puntos', 'a1estado', 'a1link', 'a2estado', 'a2link', 'a3estado', 'a3link', 'a4estado', 'a4link', 'a5estado', 'a5link', 'a6estado', 'a6link', 'a7estado', 'a7link', 'a8estado', 'a8link', 'a9estado', 'a9link', 'a10estado', 'a10link', 'a11estado', 'a11link', 'a12estado', 'a12link', 'a13estado', 'a13link', 'a14estado', 'a14link', 'a15estado', 'a15link', 'a16estado', 'a16link')
--- copatic/serializers/escuela.py
+++ copatic/serializers/escuela.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from copatic.models.escuela import Escuela
+from rest_framework_json_api.relations import ResourceRelatedField
 
 class EscuelaSerializer(serializers.ModelSerializer):
+    equipos = ResourceRelatedField(many=True, read_only=True, required=False)
+
+    included_serializers = {
+        'equipos': 'copatic.serializers.equipo.EquipoSerializer'
+    }
 
     class Meta:
         model = Escuela
-        fields = ('id', 'nombre', 'localidad', 'latitud', 'longitud')
+        fields = ('id', 'nombre', 'localidad', 'latitud', 'longitud', 'equipos')
+
+    class JSONAPIMeta:
+        included_resources = ['equipos']
